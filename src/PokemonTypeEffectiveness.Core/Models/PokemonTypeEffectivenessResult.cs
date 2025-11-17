a04ad7f0759f59527406d7e60cc14512c4f62762
claudewill1 <claude.will@yahoo.com>
2025-11-17 15:00:18 -0600

    PokemonTypeEffectivenessResult model added for the calulated strengths and weaknesses, and type information which is returned by the effectiveness service

---
+++ src/PokemonTypeEffectiveness.Core/Models/PokemonTypeEffectivenessResult.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace PokemonTypeEffectiveness.Core.Models
+{
+    // the result object returned and shown to the user
+    public class PokemonTypeEffectivenessResult
+    {
+        // The name of the Pokemon returned by the API
+        public string Pokename { get; set; } = string.Empty;
+
+        // The types that the pokemon has (e.g. fire, flying)
+        public List<string> Types { get; set; } = new();
+
+        // List of types that this pokemon is strong against
+        public List<string> StrongAgainst { get; set; } = new();
+
+        // List of types that this pokemon is weak against
+        public List<string> WeakAgainst { get; set; } = new();
+        
+    }
+}
